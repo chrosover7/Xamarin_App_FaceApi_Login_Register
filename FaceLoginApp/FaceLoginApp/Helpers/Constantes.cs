5361b7a2c1069cb43db367128ad2274950cfe974
Chrosover <55304501+chrosover7@users.noreply.github.com>
2019-11-30 15:11:02 -0500

    mi primer commit

---
+++ FaceLoginApp/FaceLoginApp/Helpers/Constantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceLoginApp.Helpers
+{
+    public static class Constantes
+    {
+        public static readonly string FaceApiURL = "https://appti.cognitiveservices.azure.com/face/v1.0";
+        public static readonly string FaceApiKey = "448b47079cd54e67b40ec531326e1be7";
+
+        public static readonly string FaceGroupID = "sistemas";
+        public static readonly string FaceGroupName = "Depto. de Sistemas";
+        public static readonly string FaceGroupDescription = "Repositorio de rostros de los empleados de Sistemas";
+
+        public static readonly string NombreBD = "BaseDatosSistemas.db";
+    }
+}
